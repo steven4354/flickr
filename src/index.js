9de9c23bf7642fde618e4be5e092c6f05c492d07
steven4354 <steven4354@gmail.com>
2017-09-26 18:25:27 -0700

    created the jsonp pull functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import $ from 'jquery-3.2.1.js'
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      tag: "chipmunks",
+      newTag: ""
+    }
+    console.log('tag', this.state.tag, 'newTag', this.state.newTag);
+  }
+
+  setTag(){
+    this.setState({tag: this.state.newTag}, function () {console.log(this.state.tag)} )
+  }
+
   render(){
     return(
-      <div>Hello World!</div>
-    )
-  }
+      <div>
+        <input
+          type='text'
+          placeholder='Enter Tag'
+          onChange={event => this.setState({newTag: event.target.value})}
+          />
+        <button type="button" className="btn btn-default"
+          onClick={() => {
+            this.setTag()
+            $.ajax({
+              url: 'https://api.flickr.com/services/feeds/photos_public.gne',
+              dataType: 'jsonp',
+              data: { "tags": this.state.newTag, "format": "json" }
+            });
+          }}
+          >
+          Search!
+        </button>
+      </div>
+  )}
+
 }
 
 ReactDOM.render(
