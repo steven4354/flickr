55fbb46096d5519734c3e6000802e04cf9dc2b94
steven4354 <steven4354@gmail.com>
2017-10-02 19:35:54 -0700

    created a history feature

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar'
 import SearchDisplay from '../containers/search_display'
+import HistoryButton from '../containers/history_button'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <HistoryButton />
         <SearchDisplay />
       </div>
     );
---
+++ src/containers/history_button.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+
+class HistoryButton extends Component {
+
+  render(){
+    if(!this.props.photos[0]){
+      console.log('this is the undefined history', this.props.photos[0]);
+      return(
+        <div>No search history yet!</div>
+      )
+    }
+    else if (this.props.photos.length == 2) {
+      return(
+        <div>
+          Here are you're recent searches:
+          <ul>
+            <li>{this.props.photos[0].title}</li>
+          </ul>
+        </div>
+      )
+    }
+    else if (this.props.photos.length == 3) {
+      return(
+        <div>
+          Here are you're recent searches:
+          <ul>
+            <li>{this.props.photos[0].title}</li>
+            <li>{this.props.photos[1].title}</li>
+          </ul>
+        </div>
+      )
+    }
+
+    return(
+      <div>
+        Here are you're recent searches:
+        <ul>
+          <li>{this.props.photos[0].title}</li>
+          <li>{this.props.photos[1].title}</li>
+          <li>{this.props.photos[2].title}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    photos: state.photos
+  }
+}
+
+export default connect(mapStateToProps)(HistoryButton)
